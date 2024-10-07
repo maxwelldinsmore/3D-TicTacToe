668e02bf535ade2777693b1131fa43d36c3feddb
A3lin <maxwell.dinsmore@dcmail.ca>
2024-10-07 13:14:34 -0400

    Add project files.

---
+++ COSC2100_A2_MaxDinsmore/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace COSC2100_A2_MaxDinsmore
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
