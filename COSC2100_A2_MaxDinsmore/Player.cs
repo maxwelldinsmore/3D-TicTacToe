43c3342260db470c283c23e7b13dee62627af0ca
Maxwell Dinsmore <maxwell.dinsmore@dcmail.ca>
2024-10-08 15:22:45 -0400

    created initial grid and player object

---
+++ COSC2100_A2_MaxDinsmore/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COSC2100_A2_MaxDinsmore
+{
+    internal class Player(string name)
+    {
+        string playerName = name;
+        List<int> pieces = [3,3,3];
+        
+    }
+}
