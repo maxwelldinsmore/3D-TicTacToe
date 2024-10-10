8390a6047f5e945d60e8fe48d595c109b6034489
Maxwell Dinsmore <maxwell.dinsmore@dcmail.ca>
2024-10-10 17:13:14 -0400

    Generates Grid fully and beginning to add player functionality

--- COSC2100_A2_MaxDinsmore/MainWindow.xaml.cs
+++ COSC2100_A2_MaxDinsmore/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,15 +19,61 @@ namespace COSC2100_A2_MaxDinsmore
     
     public partial class MainWindow : Window
     {
-        // Default grid size
-        int gridSize = 3;
+        // variables
+        int gridSize;
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        // either hides all main menu items or makes them visible
+        private void hideMenuItems()
         {
+            if (buttonStartGame.Visibility == Visibility.Visible)
+            {
+                this._3SizeRadioButton.Visibility = Visibility.Hidden;
+                this._4SizeRadioButton.Visibility = Visibility.Hidden;
+                this.playerCountTextBox.Visibility = Visibility.Hidden;
+                this.buttonStartGame.Visibility = Visibility.Hidden;
+                this.titleLabel.Visibility = Visibility.Hidden;
+                this.playerLabel.Visibility = Visibility.Hidden;
+                this.gridSizeLabel.Visibility = Visibility.Hidden;
+            } else
+            {
+                this._3SizeRadioButton.Visibility = Visibility.Visible;
+                this._4SizeRadioButton.Visibility = Visibility.Visible;
+                this.playerCountTextBox.Visibility = Visibility.Visible;
+                this.buttonStartGame.Visibility = Visibility.Visible;
+                this.titleLabel.Visibility = Visibility.Visible;
+                this.playerLabel.Visibility = Visibility.Visible;
+                this.gridSizeLabel.Visibility = Visibility.Visible;
+            }
+            
+        }
+
+        private void buttonStartGame_Click(object sender, RoutedEventArgs e)
+        {
+            int players = 0;
+            int.TryParse(this.playerCountTextBox.Text, out players);
+            if (players < 2 && players > 4)
+            {
+                MessageBox.Show("Error invalid amount of players!");
+                return;
+            }
+
+
+            hideMenuItems();
+            if (this._4SizeRadioButton.IsChecked == true)
+            {
+                gridSize = 4;
+            }
+            else {
+                gridSize = 3;
+            }
+            this.Width = gridSize * 168 + 400;
+            this.Height = gridSize * 168 + 40;
+
+            
 
             // Thickness referenced from stack overflow
             // https://stackoverflow.com/questions/1003772/setting-margin-properties-in-code
@@ -53,5 +100,8 @@ namespace COSC2100_A2_MaxDinsmore
                 }
             }
         }
+
+
+
     }
 }
--- COSC2100_A2_MaxDinsmore/Tile.xaml.cs
+++ COSC2100_A2_MaxDinsmore/Tile.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,9 +21,39 @@ namespace COSC2100_A2_MaxDinsmore
     /// </summary>
     public partial class Tile : UserControl
     {
+        int bigCircle = 0;
+        int mediumCircle = 0;
+        int smallCircle = 0;
         public Tile()
         {
             InitializeComponent();
+        }
+
+        private void screenClick(object sender, MouseButtonEventArgs e)
+        {
+            Point p = Mouse.GetPosition(this);
+            circleSize(p);
+        }
+
+        private void circleSize(Point point)
+        {
+            double distanceFromCenter;
+
+            point.X -= 168 / 2;
+            point.Y -= 168 / 2;
+
+            distanceFromCenter = Math.Sqrt(Math.Pow(point.X, 2) + Math.Pow(point.Y, 2));
+            MessageBox.Show(distanceFromCenter.ToString());
+            if (distanceFromCenter >= 27 && distanceFromCenter <= 43)
+            {
+
+            } else if (distanceFromCenter >= 46 && distanceFromCenter <= 61)
+            {
+
+            } else if (distanceFromCenter >= 65 && distanceFromCenter <= 79)
+            {
+
+            }
         }
     }
 }
