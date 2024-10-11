807b91ab23542c95d066e8107060d0ca2e6f35b5
Maxwell Dinsmore <maxwell.dinsmore@dcmail.ca>
2024-10-11 13:29:29 -0400

    Assigned each player a colour based off their number for now

--- COSC2100_A2_MaxDinsmore/Player.cs
+++ COSC2100_A2_MaxDinsmore/Player.cs
@@ -6,13 +6,43 @@ using System.Threading.Tasks;
 
 namespace COSC2100_A2_MaxDinsmore
 {
-    internal class Player(string name, string colour)
-    {
-        string playerName = name;
-        string playerColour = colour;
 
-        
-        
+    public class Player
+    {
+        public string colour = "";
+        public string[] images;
+        public Player() { }
+        public Player(int playerNumber)
+        {
+
+            if (playerNumber == 1)
+            {
+                this.colour = "red";
+                images = ["/Assets/redRing1", "/Assets/redRing2", "/Assets/redRing3"];
+            }
+            else if (playerNumber == 2)
+            {
+                this.colour = "purple";
+                images = ["/Assets/purpleRing1", "/Assets/purpleRing2", "/Assets/purpleRing3"];
+            }
+            else if (playerNumber == 3)
+            {
+                this.colour = "blue";
+                images = ["/Assets/blueRing1", "/Assets/blueRing2", "/Assets/blueRing3"];
+            }
+            else
+            {
+                this.colour = "green";
+                images = ["/Assets/greenRing1", "/Assets/greenRing2", "/Assets/greenRing3"];
+            }
+
+
+
+        }
+
+
+
+
     }
 
 }
